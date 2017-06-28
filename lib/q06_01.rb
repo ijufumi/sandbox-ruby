11d2298ba68a0da9cd2b38a8e36d49d7d55e8009
Takafumi Iju <takafumi.iju@sk.com>
2017-06-28 14:32:09 +0900

    Add q6

---
+++ lib/q06_01.rb
@@ -0,0 +1,42 @@
+def calculate(n)
+  m = n
+  flag = false
+  while true
+    if flag
+      if m == n or m == 1 then
+        break
+      else
+        if m.modulo(2) == 0 then
+          m = m/2
+        else
+          m = m * 3 + 1
+        end
+      end
+    else
+      if m == n then
+        flag = true
+        m = m * 3 + 1
+      else
+        if m.modulo(2) == 0 then
+          m = m/2
+        else
+          m = m * 3 + 1
+        end
+      end
+    end
+  end
+  return m
+end
+
+cnt = 0
+(2..10000).each{|i|
+  if i.modulo(2) != 0 then
+    next
+  end
+  m = calculate(i)
+  if m == i then
+    cnt = cnt + 1
+  end
+}
+
+puts cnt
---
+++ lib/q06_02.rb
@@ -0,0 +1,16 @@
+def calculate(n)
+  m = n * 3 + 1
+  while m != 1
+    if m.modulo(2) == 0 then
+      m = m/2
+    else
+      m = m * 3 + 1
+    end
+    return true if m == n
+  end
+  return false
+end
+
+puts 2.step(10000, 2).count{|i|
+  calculate(i)
+}
