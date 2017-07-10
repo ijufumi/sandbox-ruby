c126160fa0f080f83c5391f1cf261956b260b2d8
Takafumi Iju <takafumi.iju@sk.com>
2017-07-10 14:02:33 +0900

    Add q11

---
+++ lib/q1/q11_04.rb
@@ -0,0 +1,15 @@
+#
+# 全て写経
+#
+a = b = 1
+count = 0
+while (count < 11) do
+  c = a + b
+  sum = 0
+  c.to_s.split(//).each{|e| sum += e.to_i}
+  if (c % sum == 0) then
+    puts c
+    count += 1
+  end
+  a, b = b, c
+end
